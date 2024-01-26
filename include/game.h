59c24c981d9ff0ba43f12190053a6723862b9cef
Omer Tershnjaku <omer.tershnjaku05@htl-salzburg.ac.at>
2024-01-26 09:45:33 +0100

    Dodge and parry

---
+++ include/game.h
@@ -0,0 +1,27 @@
+//
+// Created by 1764331 G731GU-EV261 on 23.01.2024.
+//
+
+#ifndef SFML_PROJECT_GAME_H
+#define SFML_PROJECT_GAME_H
+#include "SFML/Graphics.hpp"
+#include "SFML/Window.hpp"
+const int window_width=1400;
+const int window_height=800;
+
+//sf::IntRect *recttit= reinterpret_cast<sf::IntRect *>(0, 0, 0, 0);
+//sf::Image * image;
+/*
+class player{
+
+
+private:
+
+public:
+
+};
+*/
+
+
+
+#endif //SFML_PROJECT_GAME_H
---
+++ src/game.cpp
@@ -0,0 +1,5 @@
+//
+// Created by 1764331 G731GU-EV261 on 23.01.2024.
+//
+#include "game.h"
+
